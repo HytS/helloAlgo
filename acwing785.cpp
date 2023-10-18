40ad234b4e9f17bcf582ab5cdb3ff82bf5ced090
houshuai <m17856943636@163.com>
2023-10-18 19:52:59 +0800

    更新

---
+++ acwing785.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+const int N=1e8+10;
+int q[N];
+
+int n;
+//确定分界点
+//调整范围
+//递归处理左右两端
+void quick_sort(int q[],int l,int r){
+    if(l>=r) return ;
+    
+    int x=q[(l+r)/2],i=l-1,j=r+1;
+    
+    while(i<j){
+        do{i++;}while(q[i]<x);
+        do{j--;}while(q[j]>x);
+        if(i<j) swap(q[i],q[j]);
+    }
+    quick_sort(q,l,j);
+    quick_sort(q,j+1,r);
+    
+    
+}
+
+int main(){
+    
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&q[i]);
+    }
+    quick_sort(q,0,n-1);
+    for(int i=0;i<n;i++)
+        printf("%d ",q[i]);
+    return 0;
+}
